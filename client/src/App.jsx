bbfdeed0b669815539d1a202ccdaae276ccea195
Gabriel Serna <109190797+gabserna@users.noreply.github.com>
2023-07-17 10:11:32 -0600

    route working

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,12 @@
-import React from "react";
 import { Routes, Route } from "react-router-dom";
-import { ExpenseTrackerApp } from "./components/ExpenseTrackerApp";
-import "bootstrap/dist/css/bootstrap.min.css";
+import { Login } from "./components/Login";
+// import "bootstrap/dist/css/bootstrap.min.css";
 
 // renders the component inside the Route element to be used in the app's routes
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<ExpenseTrackerApp />} />
+      <Route path="/" element={<Login />} />
     </Routes>
   );
 }
