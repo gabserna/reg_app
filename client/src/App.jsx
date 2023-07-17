a74882a4fbc0645565cee462afc1513fc94211f7
Gabriel Serna <109190797+gabserna@users.noreply.github.com>
2023-07-17 09:15:10 -0600

    path created and files relocated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,42 +1,14 @@
 import React from "react";
-import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import { ExpenseTrackerApp } from "./components/ExpenseTrackerApp";
+import "bootstrap/dist/css/bootstrap.min.css";
 
+// renders the component inside the Route element to be used in the app's routes
 function App() {
   return (
-    <>
-    <div className="main">
-      <h1 id="login-header">admin page</h1>
-
-      
-      <table>
-        <thead>
-        <th>ID</th>
-        <th>Last Name</th>
-        <th>First Name</th>
-        <th>Telephone</th>
-        <th>Address</th>
-        <th>Courses</th>
-        <th>Changes</th>
-        </thead>
-        <tbody>
-        <td>id</td>
-        <td>last</td>
-        <td>first</td>
-        <td>tel</td>
-        <td>address</td>
-        <td>courses</td>
-        <td><button>Edit</button>
-        <button>Save</button>
-        <button>Delete</button></td>
-        </tbody>
-      </table>
-      <button>Create New User</button>
-      
-    
-      
-    </div>
-    </>
+    <Routes>
+      <Route path="/" element={<ExpenseTrackerApp />} />
+    </Routes>
   );
 }
-
 export default App;
