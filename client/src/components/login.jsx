08d53f834f8c042a28039aaa2b1ab2d1a2158270
Gabriel Serna <gabriel.sernapoot6740@stu.mtec.edu>
2023-07-20 02:25:08 -0600

    dark mode implemented

--- client/src/components/login.jsx
+++ client/src/components/login.jsx
@@ -1,20 +1,54 @@
+import React, { useState } from "react";
 import "../Style.css";
-//import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMoon } from '@fortawesome/free-solid-svg-icons'
+
+function darkMode() {
+  // Get the current state of dark mode from localStorage (if any)
+  const isDarkMode = localStorage.getItem("darkMode") === "true";
+
+  // Toggle the dark mode
+  const newDarkMode = !isDarkMode;
+
+  // Save the new dark mode state to localStorage
+  localStorage.setItem("darkMode", newDarkMode);
+
+  // Update the class of the document.body based on the new dark mode state
+  document.body.classList.toggle("dark-mode", newDarkMode);
+}
 
 function Login() {
+  // State to track the username and password fields (if needed in the future)
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  // Handle form submission (if needed in the future)
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Add your login logic here if necessary
+  };
+
   return (
     <>
+    {/* Use onClick to call the darkMode function */}
+    <button id="darkModeButton" onClick={darkMode} aria-label="Toggle dark mode">
+        <FontAwesomeIcon icon={faMoon} />
+      </button>
+
       <div className="main">
         <h1 id="login-header">Welcome!</h1>
-        <h4 id="login-header">Login with your user credentials</h4>
+        <h4 id="login-subheader">Login with your user credentials</h4>
 
-        <form id="login-form">
+        <form id="login-form" onSubmit={handleSubmit}>
           <input
             type="text"
             name="username"
             id="username-field"
             className="login-form-field"
             placeholder="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
           />
           <br />
           <input
@@ -23,22 +57,18 @@ function Login() {
             id="password-field"
             className="login-form-field"
             placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           />
           <br />
           <input type="submit" value="Login" id="login-form-submit" />
         </form>
-      {/* 
+
         <p>
-          New here?, click:{" "}
-          <Link to="/register">Register</Link>
+          New here? Click: <Link to="/register">Register</Link>
         </p>
-
-        // try and catch statements as an event handlers
-        */}
-
       </div>
     </>
   );
 }
-
 export default Login;
