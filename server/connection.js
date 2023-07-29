add549f812bc6aebe44559e3dc086c25e913d6fc
Gabriel Serna <gabriel.sernapoot6740@stu.mtec.edu>
2023-07-29 00:39:38 -0600

    Revert "Merge remote-tracking branch 'origin/kevin'"
    This reverts commit a7e050d19558852fd5c728dc2321fcd7cb06e59d, reversing
    changes made to 31036849042db505735151311f0a76fc72c07ac2.

--- server/connection.js
+++ server/connection.js
@@ -1,2 +1,14 @@
+const {Client} = require('pg');
+require("dotenv").config();
+
+
+const client = new Client({
+    host: process.env.PGHOST,
+    user: process.env.PGUSER,
+    port: process.env.PGPORT,
+    password: process.env.PGPASSWORD,
+    database: process.env.PGDATABASE,
+    ssl: true
+});
 
 module.exports = client
