007c0d8c183ff54a4360fe995ffff1bf50a8a284
Kevin Warner <kevin.warner520@gmail.com>
2023-07-31 09:58:39 -0600

    changed config.js. updated to the server.js

--- server/server.js
+++ server/server.js
@@ -2,20 +2,26 @@ const client = require("./connection.js");
 const express = require("express");
 const path = require("path");
 
-
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
 const bodyParser = require("body-parser");
 const { cli } = require("winston/lib/winston/config/index.js");
+
 app.use(bodyParser.json());
 
 // Have Node serve the files for our built React app
 app.use(express.static(path.resolve(__dirname, "../client/dist")));
 
+// parse requests of content-type - application/json
+app.use(express.json())
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
 // Handle GET requests to /api route
-app.get("/api", (req, res) => {
+app.get("/classes", (req, res) => {
   client.query(`Select * from class_info`, (err, result)=>{
     if(!err){
         res.send(result.rows);
@@ -23,6 +29,11 @@ app.get("/api", (req, res) => {
   });
   client.end;
 });
+
+app.get("/", (req, res) => {
+  res.json({ message: "hello from the server" });
+});
+
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
