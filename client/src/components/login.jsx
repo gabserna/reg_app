8b80258bd56e6576552b77f5209652cdf5413754
Gabriel Serna <gabriel.sernapoot6740@stu.mtec.edu>
2023-08-02 00:39:33 -0600

    spinner running, need 2b  called correctly

--- client/src/components/login.jsx
+++ client/src/components/login.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import "../Style.css";
 import { Link } from "react-router-dom";
 import NavBar from "./Navbar";
+import LoadingSpinner from "./spinner"
 
 function Login() {
   // State to track the username and password fields (if needed in the future)
@@ -27,6 +28,7 @@ function Login() {
 
   return (
     <>
+    <LoadingSpinner />
       <div className="main">
         <h1 id="login-header">Welcome!</h1>
         <h4 id="login-subheader">Login with your user credentials</h4>
