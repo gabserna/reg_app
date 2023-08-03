eb4bb3fced78eaac823de1156ccddbfb533d1777
Gabriel Serna <gabriel.sernapoot6740@stu.mtec.edu>
2023-08-02 23:45:07 -0600

    classes json added, but just locally

--- client/src/components/courses.jsx
+++ client/src/components/courses.jsx
@@ -1,12 +1,12 @@
-import React, { useState, useEffect } from "react";
-//import NavBar from "./Navbar";
+import { useState, useEffect } from "react";
+import NavBar from "./Navbar";
 import "../Style.css";
 
-function App() {
+function Courses() {
   const [classesData, setClassesData] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:3001/classes")
+    fetch("./classes")    //how to call from server port 3001 ??
       .then((response) => response.json())
       .then((data) => setClassesData(data))
       .catch((error) => console.error("Error fetching data:", error));
@@ -14,22 +14,10 @@ function App() {
 
   return (
     <>
-      <NavBar />
       <div className="main">
         <h1 id="login-header">Courses</h1>
         <table>
-          <thead>
-            <tr>
-              <th>Course ID</th>
-              <th>Course Title</th>
-              <th>Course Description</th>
-              <th>Classroom Number</th>
-              <th>Capacity</th>
-              <th>Credit Hours</th>
-              <th>Tuition Cost</th>
-              <th>Actions</th>
-            </tr>
-          </thead>
+
           <tbody>
             {classesData.map((classItem, index) => (
               <tr key={index}>
@@ -53,4 +41,4 @@ function App() {
   );
 }
 
-export default App;
+export default Courses;
