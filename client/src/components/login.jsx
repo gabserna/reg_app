18f041456ac1d4aa8000c5a282944f263b038481
Gabriel Serna <gabriel.sernapoot6740@stu.mtec.edu>
2023-08-06 00:34:28 -0600

    show passwd button modified

--- client/src/components/login.jsx
+++ client/src/components/login.jsx
@@ -1,80 +1,67 @@
 import React, { useState } from "react";
-import "../Style.css";
 import { Link } from "react-router-dom";
-import LoadingSpinner from "./spinner"
+import LoadingSpinner from "./spinner"; // Mantengo esto asumiendo que se usa en otro lugar del componente o proyecto
+import "font-awesome/css/font-awesome.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../Style.css";
 
 function Login() {
-  // State to track the username and password fields (if needed in the future)
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const [showPassword, setShowPassword] = useState(false);
+  const [passwordShown, setPasswordShown] = useState(false); // Combine `showPassword` y `passwordShown` en una sola variable
   const [isLoading, setIsLoading] = useState(false);
 
-  // Handle form submission (if needed in the future)
+  // Handle form submission
   const handleSubmit = (event) => {
     event.preventDefault();
-    // Add your login logic here if necessary
+    // login logic here
   };
 
-  function handleShowPassword() {
-    setShowPassword(!showPassword);
+  function togglePassword() {
+    setPasswordShown(!passwordShown);
   }
-
-  //NOTE: hamburger menu on responsive?
-
-  // spinner
-  //step 4 of      https://contactmentor.com/how-to-add-loading-spinner-react-js/?expand_article=1
 
   return (
     <>
-    <LoadingSpinner />
+      <LoadingSpinner />
       <div className="main">
-        <h1 id="login-header">Welcome!</h1>
-        <h4 id="login-subheader">Login with your user credentials</h4>
+        <h2>Welcome!</h2>
+        <h5>Login with your user credentials</h5>
 
         <form id="login-form" onSubmit={handleSubmit}>
           <input
             type="text"
             name="username"
-            id="username-field"
+            id="userLogin"
             className="login-form-field"
             placeholder="Username"
             value={username}
             onChange={(e) => setUsername(e.target.value)}
           />
           <br />
-          <input
-            type={showPassword ? "text" : "password"}
-            name="password"
-            id="myPasswd"
-            className="login-form-field"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <br />
-          <input type="submit" value="Login" id="login-form-submit" />
-          <label>
+          <div className="hidePasswd">
             <input
-              type="checkbox"
-              checked={showPassword}
-              onChange={handleShowPassword}
-            />{" "}
-            show
-          </label><br />
-          <button type="submit" data-provider="google">
-          <span className="datt" data-provider="google"></span>
-          <span className="cba09aff6">Continue with Google</span>
-          </button>
-          {/* <a href="/">Continue with Google</a> */}
+              type={passwordShown ? "text" : "password"}
+              name="password"
+              id="userPwd"
+              className="login-form-field"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <button onClick={togglePassword}>
+              <i
+                className={`fa ${passwordShown ? "fa-eye" : "fa-eye-slash"}`}
+              />
+            </button>
+          </div>
+          <input type="submit" id="login-form-submit" />
         </form>
-
-        <p>
+        <label>
           New here? Click: <Link to="/register">Register</Link>
-        </p>
+        </label>
       </div>
     </>
   );
 }
-
 export default Login;
