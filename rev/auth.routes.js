755247cb585d5fdc9413a067f51a2d4eeff2dbd6
Gabriel Serna <gabriel.sernapoot6740@stu.mtec.edu>
2023-08-06 22:50:28 -0600

    rev folder removed

--- rev/auth.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const authController = require('../controllers/auth.controller');
-const router = express.Router(); //eslint-disable-line new-cap
-
-router.post('/register', authController.register);
-router.post('/login', authController.login);
-
-module.exports = router;
