e9934f352d89eb5deb5d523e009773485e50be46
Gabriel Serna <gabriel.sernapoot6740@stu.mtec.edu>
2023-08-11 22:35:32 -0600

    text adapted

--- client/src/pages/notFoundPage.jsx
+++ client/src/pages/notFoundPage.jsx
@@ -5,7 +5,7 @@ function NotFoundPage() {
   return (
     <div>
       <h1>404 - Not Found</h1>
-      <p>The page you're looking for doesn't exist.</p>
+      <p>The page you&apos;re looking for doesn&apos;t exist.</p>
       <img id="notfound404" src={notFound} alt="" />
     </div>
   );
