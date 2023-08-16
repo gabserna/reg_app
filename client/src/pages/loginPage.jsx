80ba29ebc875ac6e884491bae0ebc936ae394deb
Gabriel Serna <gabriel.sernapoot6740@stu.mtec.edu>
2023-08-16 01:14:24 -0600

    login attempt

--- client/src/pages/loginPage.jsx
+++ client/src/pages/loginPage.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import TogglePassword from "../components/TogglePassword"; // Import el nuevo componente
 import "font-awesome/css/font-awesome.min.css";
@@ -7,6 +7,20 @@ import "../styles/GlobalStyles.css";
 function Login() {
   const [username, setUsername] = useState("");
   const [passwordShown, setPasswordShown] = useState(false);
+  const [serverData, setServerData] = useState(null);
+
+  useEffect(() => {
+    // Perform an HTTP GET request to your server when the component mounts
+    fetch("/login")
+      .then((response) => response.json())
+      .then((data) => {
+        setServerData(data); // Update the state with the fetched data
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []); //Empty dependency array to fetch data only once when the component mounts
+
 
   // Handle form submission
   const handleSubmit = (event) => {
@@ -52,6 +66,10 @@ function Login() {
             New here? Click: <Link to="/registration">Register</Link>
           </label>
         </form>
+
+        {/* Render the fetched data */}
+        {serverData && <p>Server Data: {serverData.someProperty}</p>}
+
       </div>
     </>
   );
